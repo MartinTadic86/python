524320b22a217536d2da8ed311e96c7fa5e31c80
Martin Tadič <96323454+MartinTadic86@users.noreply.github.com>
2023-05-03 13:07:22 +0200

    Update fdkubfeuwsbdeiupéro.py

--- fdkubfeuwsbdeiupéro.py
+++ fdkubfeuwsbdeiupéro.py
@@ -7,12 +7,12 @@ from tkinter import ttk
 from tkinter.messagebox import showinfo
 def testfunction():
     showinfo(
-        message='Hallo dis is Jamal from mikrosoft" " hau ar jú'    
+        message='Hallo dis is Jamal from mikrosoft hau ar jú'    
     )
 root = Tk()
 root.geometry("250x250")
 frm = ttk.Frame(root, padding=60)
 frm.grid()
-ttk.Label(frm, text="secret hacker stuff").grid(column=0, row=1)
-ttk.Button(frm, text="secret hacker stuff.péro", command=testfunction).grid(column=0, row=2)
+ttk.Label(frm, text="secret_hacker_stuff.exe").grid(column=0, row=1)
+ttk.Button(frm, text="open secret hacker stuff", command=testfunction).grid(column=0, row=2)
 root.mainloop()
